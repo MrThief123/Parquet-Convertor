d4b22f7fc2e27bc5cc4e42eac9b577a3ec0ba840
MrThief123 <90836480+MrThief123@users.noreply.github.com>
2025-12-20 16:16:34 +1100

    fix: add lambda layer for dependencies

--- Parquet-Convertor/lib/parquet-convertor-stack.ts
+++ Parquet-Convertor/lib/parquet-convertor-stack.ts
@@ -3,33 +3,43 @@ import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as s3n from 'aws-cdk-lib/aws-s3-notifications';
+import * as path from 'path';
 
 export class ParquetConvertorStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // Input bucket
-    const inputBucket = new s3.Bucket(this, 'CsvInputBucket', {
+    const inputBucket = new s3.Bucket(this, 'Parquet-Convert-InputBucket-', {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
     });
 
     // Output bucket
-    const outputBucket = new s3.Bucket(this, 'ParquetOutputBucket', {
+    const outputBucket = new s3.Bucket(this, 'Parquet-Convert-OutputBucket-', {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
+    });
+
+    // Lambda layer for parquet library
+    const parquetLayer = new lambda.LayerVersion(this, 'ParquetLayer', {
+      code: lambda.Code.fromAsset(path.join(__dirname, '../layer')),
+      compatibleRuntimes: [lambda.Runtime.PYTHON_3_10],
     });
 
     // Lambda function
     const converterFn = new lambda.Function(this, 'CsvToParquetFn', {
       runtime: lambda.Runtime.PYTHON_3_10,
-      handler: 'handler.main',
-      code: lambda.Code.fromAsset('lambda/conversion'),
+      handler: 'handler.handler',
+      code: lambda.Code.fromAsset(
+        path.join(__dirname, '../lambda/conversion')
+      ),
+      layers: [parquetLayer],
       environment: {
         OUTPUT_BUCKET: outputBucket.bucketName,
       },
       timeout: cdk.Duration.seconds(60),
-      memorySize: 1024,
+      memorySize: 2048,
     });
 
     // Permissions
